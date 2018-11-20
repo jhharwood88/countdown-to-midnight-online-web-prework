8cdf968e3665aec5a46cb320b76e06aa3fa31090
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-20 20:28:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,16 @@
 def countdown(number)
- number = 10
   while number > 0 do
+    if number == 1
+      puts "#{number} Seconds"
+    else
     puts "#{number} SECOND(S)!"
     number -= 1
+  end
   end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
-  number = 10
   while number > 0 do
     sleep(1)
     puts "#{number} SECOND(S)!"
@@ -16,3 +18,4 @@ def countdown_with_sleep(number)
   end
   return "HAPPY NEW YEAR!"
 end
+countdown(10)
